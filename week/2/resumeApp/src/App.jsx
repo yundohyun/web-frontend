2c27f1b1ca4b106a2c0cc9cb388a24b295a8384b
yundohyun <to@dohyun.it>
2025-03-13 05:25:11 +0000

    feat: add react project in `/week/2/resumeApp`

---
+++ week/2/resumeApp/src/App.jsx
@@ -0,0 +1,16 @@
+import Privacy from "./components/Privacy";
+
+function App() {
+  return (
+    <>
+      <h1>이력서</h1>
+      <Privacy
+        name={"윤도현"}
+        phone={"010-1234-5678"}
+        email={"yundohyun050121@gmail.com"}
+      />
+    </>
+  );
+}
+
+export default App;
