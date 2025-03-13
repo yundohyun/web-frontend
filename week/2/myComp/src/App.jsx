437def06661a45ba9960fe33da25d64082d3cec9
yundohyun <to@dohyun.it>
2025-03-13 05:13:53 +0000

    feat: add react project in `/week/2/myComp`

---
+++ week/2/myComp/src/App.jsx
@@ -0,0 +1,16 @@
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+      <div>안녕, 리액트!</div>
+    </>
+  );
+}
+
+export default App;
