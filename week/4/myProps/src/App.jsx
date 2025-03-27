546086865f36461bece379b24d8c4773173fed86
yundohyun <to@dohyun.it>
2025-03-27 14:33:29 +0900

    feat: add react project in `/week/4/myProps`

---
+++ week/4/myProps/src/App.jsx
@@ -0,0 +1,13 @@
+import Button from "./components/Button";
+
+function App() {
+  return (
+    <>
+      <Button text="메일" style={{ color: "red" }} />
+      <Button text="카페" style={{ color: "blue" }} />
+      <Button text="블로그" style={{ color: "blue" }} />
+    </>
+  );
+}
+
+export default App;
---
+++ week/4/myProps/src/components/Button.jsx
@@ -0,0 +1,5 @@
+const Button = ({ text, style }) => {
+  return <button style={style}>{text}</button>;
+};
+
+export default Button;
