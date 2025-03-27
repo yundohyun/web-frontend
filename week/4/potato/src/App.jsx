af690d524abbc1a15f61a25e97162e1ca676a917
yundohyun <to@dohyun.it>
2025-03-27 14:35:18 +0900

    feat: add react project in `/week/4/potato`

---
+++ week/4/potato/src/App.jsx
@@ -0,0 +1,15 @@
+import Potato from "./components/Potato";
+
+function App() {
+  return (
+    <>
+      <Potato />
+      <Potato />
+      <Potato />
+      <Potato />
+      <Potato />
+    </>
+  );
+}
+
+export default App;
---
+++ week/4/potato/src/components/Potato.jsx
@@ -0,0 +1,5 @@
+const Potato = () => {
+  return <h3>I love potato</h3>;
+}
+
+export default Potato;
