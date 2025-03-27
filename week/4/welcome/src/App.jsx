2d66fcc1149280203463fb8a45c17613dbb28eb2
yundohyun <to@dohyun.it>
2025-03-27 14:33:12 +0900

    feat: add react project in `/week/4/welcome`

---
+++ week/4/welcome/src/App.jsx
@@ -0,0 +1,13 @@
+import Welcome from "./components/Welcome";
+
+function App() {
+  return (
+    <>
+      <Welcome name="Alice" />
+      <Welcome name="Bob" />
+      <Welcome name="Charlie" />
+    </>
+  );
+}
+
+export default App;
---
+++ week/4/welcome/src/components/Welcome.jsx
@@ -0,0 +1,5 @@
+const Welcome = ({ name }) => {
+  return <h1>Hello, {name}!</h1>;
+};
+
+export default Welcome;
