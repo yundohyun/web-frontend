8af505057de764de581bc57a4b4c1678cb6fffc2
yundohuun <160732611+yundohyun@users.noreply.github.com>
2025-06-12 04:19:41 +0000

    feat: add react project in `/week/12/shopApp`

---
+++ week/12/shopApp/src/Home.jsx
@@ -0,0 +1,15 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+
+
+const Home = () => {
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={}
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+};
